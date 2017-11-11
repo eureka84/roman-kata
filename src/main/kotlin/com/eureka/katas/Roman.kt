f02a489f7d1de847f334e7a0ae83c3136b207fd2
Angelo Sciarra <asciarra@byte-code.com>
2017-11-11 09:36:30 +0100

    handle the numbers from 1 to 3

--- src/main/kotlin/com/eureka/katas/Roman.kt
+++ src/main/kotlin/com/eureka/katas/Roman.kt
@@ -2,8 +2,22 @@ package com.eureka.katas
 
 class Roman(decimal: Int) {
 
+    private val symbols = listOf(Pair(1, "I"))
+
+    private val value: String by lazy {
+        var remaining = decimal
+        var roman = ""
+        symbols.forEach { (asInt, asString) ->
+            while (remaining >= asInt) {
+                roman += asString
+                remaining -= asInt
+            }
+        }
+        return@lazy roman
+    }
+
     override fun toString(): String {
-        return "I"
+        return value
     }
 
 }
--- src/test/java/com/eureka/katas/RomanSpec.kt
+++ src/test/java/com/eureka/katas/RomanSpec.kt
@@ -10,12 +10,19 @@ class RomanSpec: StringSpec() {
     init {
 
         "should convert 1" {
-
             val roman = Roman(1)
-
             roman.toString() shouldBe "I"
         }
 
+        "should convert 2" {
+            val roman = Roman(2)
+            roman.toString() shouldBe "II"
+        }
+
+        "should convert 3" {
+            val roman = Roman(3)
+            roman.toString() shouldBe "III"
+        }
     }
 }
 
