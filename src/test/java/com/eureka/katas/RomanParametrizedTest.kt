6cdbef62115730a098a26ffec7371ebb14ee833b
Angelo Sciarra <asciarra@byte-code.com>
2017-11-11 17:36:27 +0100

    added some more tests

--- src/test/java/com/eureka/katas/RomanParametrizedTest.kt
+++ src/test/java/com/eureka/katas/RomanParametrizedTest.kt
@@ -13,9 +13,30 @@ import org.junit.runner.RunWith
 class RomanParametrizedTest {
 
     @Test
-    @Parameters("1, I", "2, II", "3, III", "4, IV", "5, V",
-            "8, VIII", "9, IX", "10, X", "14, XIV", "15, XV",
-            "1944, MCMXLIV", "1943, MCMXLIII")
+    @Parameters(
+            "0, ",
+            "1, I",
+            "2, II",
+            "3, III",
+            "4, IV",
+            "5, V",
+            "8, VIII",
+            "9, IX",
+            "10, X",
+            "14, XIV",
+            "15, XV",
+            "40, XL",
+            "50, L",
+            "90, XC",
+            "100, C",
+            "400, CD",
+            "500, D",
+            "900, CM",
+            "1000, M",
+            "691, DCXCI",
+            "431, CDXXXI",
+            "1944, MCMXLIV",
+            "1943, MCMXLIII")
     fun convert(decimal: Int, roman: String) {
         Roman(decimal).toString() shouldEqual roman
     }
